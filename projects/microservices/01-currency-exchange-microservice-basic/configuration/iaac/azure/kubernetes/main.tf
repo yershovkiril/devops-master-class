f14cd2756ee47ef64888ebfe84ae5272760f09f6
Kiril <dd041091eke@gmail.com>
2020-04-05 21:43:23 +0300

    update

--- projects/microservices/01-currency-exchange-microservice-basic/configuration/iaac/azure/kubernetes/main.tf
+++ projects/microservices/01-currency-exchange-microservice-basic/configuration/iaac/azure/kubernetes/main.tf
@@ -35,6 +35,8 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
 
 terraform {
   backend "azurerm" {
+    version = "~>2.0.0"
+    features {}
     # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
     # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
     # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
